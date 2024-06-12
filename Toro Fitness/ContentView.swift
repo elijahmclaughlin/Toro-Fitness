3dd61a5440b3a040b3525cba1de2fd2826eef10a
elijahmclaughlin <86069410+elijahmclaughlin@users.noreply.github.com>
2024-06-12 19:07:21 -0400

    Initial Commit

---
+++ Toro Fitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toro Fitness
+//
+//  Created by Elijah McLaughlin on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toro Fitness/Toro_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toro_FitnessApp.swift
+//  Toro Fitness
+//
+//  Created by Elijah McLaughlin on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Toro_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
