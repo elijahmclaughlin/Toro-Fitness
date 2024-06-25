a6908d2d8c79edcbcb914b666a2f92cf07e26a4c
elijahmclaughlin <86069410+elijahmclaughlin@users.noreply.github.com>
2024-06-25 15:05:39 -0400

    Create MealsView.swift

---
+++ Toro Fitness/Preview Content/Views/Meals/MealsView.swift
@@ -0,0 +1,20 @@
+//
+//  MealsView.swift
+//  Toro Fitness
+//
+//  Created by Elijah McLaughlin on 6/12/24.
+//
+
+import SwiftUI
+
+struct MealsView: View {
+    var body: some View {
+        Text("Meals View")
+    }
+}
+
+struct MealsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealsView()
+    }
+}
