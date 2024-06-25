3a0834a2fd2f094189e7d0b0a7736e9acecf2045
elijahmclaughlin <86069410+elijahmclaughlin@users.noreply.github.com>
2024-06-25 15:05:57 -0400

    Create StoreUserSetData+CoreDataClass.swift

---
+++ WorkoutData/StoreUserSetData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StoreUserSetData+CoreDataClass.swift
+//  Toro Fitness
+//
+//  Created by Elijah McLaughlin on 6/25/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StoreUserSetData)
+public class StoreUserSetData: NSManagedObject {
+
+}
