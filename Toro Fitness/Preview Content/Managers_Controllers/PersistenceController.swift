4baef41be5ccf2fc97539750670ea1268c949624
elijahmclaughlin <86069410+elijahmclaughlin@users.noreply.github.com>
2024-06-25 15:05:15 -0400

    Create PersistenceController.swift

---
+++ Toro Fitness/Preview Content/Managers_Controllers/PersistenceController.swift
@@ -0,0 +1,39 @@
+//
+//  PersistenceController.swift
+//  Toro Fitness
+//
+//  Created by Elijah McLaughlin on 6/24/24.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "UserWorkoutsModel") // Ensure this matches your model name
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+    }
+
+    func saveContext() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
